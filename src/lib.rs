d30636c79bd8197c40b1d5b2b564b3cf997d167a
Jiangqiu Shen <ivy22233qiu@live.com>
2022-05-02 20:37:42 -0400

    fix bug

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod client;
-use client::app::SendButton;
-use wasm_bindgen::prelude::*;
-use yew::prelude::*;
-#[wasm_bindgen]
-pub fn run()->Result<(),JsValue> {
-    yew::start_app::<SendButton>();
-    Ok(())
-}
